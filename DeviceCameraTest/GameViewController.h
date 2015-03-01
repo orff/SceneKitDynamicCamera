aec7a98b868bd93bcd86e773d72bc49a8b676bda
orff <michaelhill@gmailcom>
2015-03-01 12:19:01 -0500

    Initial Commit

---
+++ DeviceCameraTest/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  DeviceCameraTest
+//
+
+//  Copyright (c) 2015 Michael Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
